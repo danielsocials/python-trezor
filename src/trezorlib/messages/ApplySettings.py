55219025e4edf3f57180ca540111b044d2f3ea88
danielsocials <daniel.socials@gmail.com>
2020-05-06 15:44:40 +0800

    add is_bixinapp flag

--- src/trezorlib/messages/ApplySettings.py
+++ src/trezorlib/messages/ApplySettings.py
@@ -4,12 +4,10 @@ from .. import protobuf as p
 
 if __debug__:
     try:
-        from typing import Dict, List, Optional
+        from typing import Dict, List  # noqa: F401
         from typing_extensions import Literal  # noqa: F401
-        EnumTypePassphraseSourceType = Literal[0, 1, 2]
     except ImportError:
-        Dict, List, Optional = None, None, None  # type: ignore
-        EnumTypePassphraseSourceType = None  # type: ignore
+        pass
 
 
 class ApplySettings(p.MessageType):
@@ -21,25 +19,27 @@ class ApplySettings(p.MessageType):
         label: str = None,
         use_passphrase: bool = None,
         homescreen: bytes = None,
-        passphrase_source: EnumTypePassphraseSourceType = None,
         auto_lock_delay_ms: int = None,
         display_rotation: int = None,
+        passphrase_always_on_device: bool = None,
         use_fee_pay: int = None,
         use_ble: bool = None,
         use_se: bool = None,
         use_exportseeds: bool = None,
+        is_bixinapp: bool = None,
     ) -> None:
         self.language = language
         self.label = label
         self.use_passphrase = use_passphrase
         self.homescreen = homescreen
-        self.passphrase_source = passphrase_source
         self.auto_lock_delay_ms = auto_lock_delay_ms
         self.display_rotation = display_rotation
+        self.passphrase_always_on_device = passphrase_always_on_device
         self.use_fee_pay = use_fee_pay
         self.use_ble = use_ble
         self.use_se = use_se
         self.use_exportseeds = use_exportseeds
+        self.is_bixinapp = is_bixinapp
 
     @classmethod
     def get_fields(cls) -> Dict:
@@ -48,11 +48,12 @@ class ApplySettings(p.MessageType):
             2: ('label', p.UnicodeType, 0),
             3: ('use_passphrase', p.BoolType, 0),
             4: ('homescreen', p.BytesType, 0),
-            5: ('passphrase_source', p.EnumType("PassphraseSourceType", (0, 1, 2)), 0),
             6: ('auto_lock_delay_ms', p.UVarintType, 0),
             7: ('display_rotation', p.UVarintType, 0),
-            8: ('use_fee_pay', p.UVarintType, 0),
-            9: ('use_ble', p.BoolType, 0),
-            10: ('use_se', p.BoolType, 0),
-            11: ('use_exportseeds', p.BoolType, 0),
+            8: ('passphrase_always_on_device', p.BoolType, 0),
+            9: ('use_fee_pay', p.UVarintType, 0),
+            10: ('use_ble', p.BoolType, 0),
+            11: ('use_se', p.BoolType, 0),
+            12: ('use_exportseeds', p.BoolType, 0),
+            13: ('is_bixinapp', p.BoolType, 0),
         }
